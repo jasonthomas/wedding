f7877badf0d48cf3256861d07ba6d4b467552e23
Jason Thomas <jason@lithiumfox.com>
2014-03-09 13:41:16 -0400

    Use Data in bulk import.

--- bulk.py
+++ bulk.py
@@ -1,42 +1,22 @@
 #!/bin/env python
-import redis
+from data import Data
 import sys
 import csv
 
-db = redis.Redis('localhost')
-
-
-def genrandom(length=6):
-    unique = False
-    while not unique:
-        key = ''.join(random.sample(string.digits, length))
-        if not getkey(key):
-            unique = True
-
-    return key
-
 
 def add(filename):
+    user = Data()
     with open(filename) as fp:
         reader = csv.reader(fp)
         for row in reader:
-            # if the length is 4 that means there is a middle name defined
-            if len(row) == 4:
-                return True
-            else
-
-
-def addvalue(firstname, lastname, guests, attending=True, middlename=None):
-    name = genrandom()
-    db.hset(name, 'firstname', firstname)
-    db.hset(name, 'lastname', lastname)
-    db.hset(name, 'middlename', middlename)
-    db.hset(name, 'guests', guests)
-    db.hset(name, 'attending', attending)
-
-
-def getkey(name):
-    return db.hkeys(name)
+            name = row[0]
+            firstname = row[1]
+            lastname = row[2]
+            middlename = row[3]
+            guests = row[4]
+            code = user.add(firstname, lastname,
+                            guests, middlename=middlename, name=name)
+            print code
 
 
 if __name__ == "__main__":
